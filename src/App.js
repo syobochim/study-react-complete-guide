c48fad15d6bb7d714174f93c7d17908e81eab6c0
syobochim <syobochim@gmail.com>
2023-02-15 23:46:28 +0900

    React Basics and Working with Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
+    { title: "Toilet Paper", amount: 94.12, date: new Date(2021, 7, 30) },
+    { title: "New Desk", amount: 450, date: new Date(2021, 5, 12) },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
