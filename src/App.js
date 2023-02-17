4c1a6f53078120956d673607e07a9ddc772d05e5
syobochim <syobochim@gmail.com>
2023-02-17 21:14:17 +0900

    Rendering Lists and Conditional Content

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+import { useState } from "react";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "001",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "002",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2021, 7, 30),
+  },
+  {
+    id: "003",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    { title: "Car Insurance", amount: 294.67, date: new Date(2021, 2, 28) },
-    { title: "Toilet Paper", amount: 94.12, date: new Date(2021, 7, 30) },
-    { title: "New Desk", amount: 450, date: new Date(2021, 5, 12) },
-  ];
-
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
   return (
     <div>
