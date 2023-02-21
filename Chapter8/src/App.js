d06a7045516ad81a452d4438e8b1f5127eaa44dd
syobochim <syobochim@gmail.com>
2023-02-21 20:02:11 +0900

    Practice Project (Chapter8)

--- Chapter8/src/App.js
+++ Chapter8/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserDataHandler = (userName, userAge) => {
+    setUsersList((prevUserList) => {
+      return [
+        ...prevUserList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-
+      <AddUser onAddUser={addUserDataHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
