8625c5940a9adcf3b23e90fd1611404e24682f68
syobochim <syobochim@gmail.com>
2023-03-02 14:53:42 +0900

    Add Header, Cart, Modal

--- Chapter11/src/components/Cart/Cart.js
+++ Chapter11/src/components/Cart/Cart.js
@@ -1,25 +1,28 @@
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
     <ul className={classes.cartItems}>
       {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+        <li key={item.id}>{item.name}</li>
       ))}
     </ul>
   );
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.button}>
-        <button className={classes.buttonAlt}>Close</button>
+        <button className={classes.buttonAlt} onClick={props.onClose}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
---
+++ Chapter11/src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import classes from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
