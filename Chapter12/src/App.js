07d0d1ba6baa2fccc92c281265f703f0b51150c1
syobochim <syobochim@gmail.com>
2023-03-10 17:08:55 +0900

    useCallback()

--- Chapter12/src/App.js
+++ Chapter12/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import "./App.css";
 import Button from "./components/UI/Button/Button";
@@ -7,15 +7,16 @@ import DemoOutput from "./components/Demo/DemoOutput";
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   console.log("DemoAPP Running");
-  const toggleParagraphHandler = () => {
+  const toggleParagraphHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
-      {/*Button を memo にしても、 toggleParagraphHandler が再描画されるので、 memoは利用されずにButtonは再描画される*/}
+      {/* Button を memo にしても、 toggleParagraphHandler が再描画されるので、 memoは利用されずにButtonは再描画される*/}
+      {/* toggleParagraphHandler を useCallback に包むことで、再作成されずに memo が再描画されなくなる*/}
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
