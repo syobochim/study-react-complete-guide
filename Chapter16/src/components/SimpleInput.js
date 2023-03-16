7f6fd4181b71d5a937bf4407936297b3a5e5bfef
syobochim <syobochim@gmail.com>
2023-03-16 20:28:41 +0900

    ユーザーInputを取得する２つの方法

--- Chapter16/src/components/SimpleInput.js
+++ Chapter16/src/components/SimpleInput.js
@@ -1,9 +1,31 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const forSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={forSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
