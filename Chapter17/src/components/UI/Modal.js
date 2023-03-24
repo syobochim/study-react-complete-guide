f57e97b959763a6d79a99710f9bf9fcfaa7a5f70
syobochim <syobochim@gmail.com>
2023-03-24 16:22:27 +0900

    Adding a Checkout Form

--- Chapter17/src/components/UI/Modal.js
+++ Chapter17/src/components/UI/Modal.js
@@ -1,10 +1,9 @@
-import { Fragment } from 'react';
-import ReactDOM from 'react-dom/client';
-
-import classes from './Modal.module.css';
+import { Fragment } from "react";
+import classes from "./Modal.module.css";
+import { createPortal } from "react-dom";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} onClick={props.onClose}/>;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
@@ -15,13 +14,13 @@ const ModalOverlay = (props) => {
   );
 };
 
-const portalElement = document.getElementById('overlays');
+const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
   return (
     <Fragment>
-      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
-      {ReactDOM.createPortal(
+      {createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
+      {createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
       )}
