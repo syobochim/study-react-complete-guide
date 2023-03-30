ffd4f9947225707cceeeb69178a61a12cc055300
syobochim <syobochim@gmail.com>
2023-03-30 20:30:58 +0900

    Multiple slice using Redux ant Toolkit

--- Chapter18/src/store/index.js
+++ Chapter18/src/store/index.js
@@ -1,10 +1,10 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
   name: "counter", // 必須
-  initialState,
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -21,10 +21,28 @@ const counterSlice = createSlice({
   },
 });
 
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
 const store = configureStore({
-  reducer: counterSlice.reducer,
-  // reducer: { counter: counterSlice.reducer },  // 複数のreducerを管理したい時
+  // reducer: counterSlice.reducer,  // reducerが一つの場合
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer }, // 複数のreducerを管理したい時
 });
 
 export const counterAction = counterSlice.actions;
+export const authAction = authSlice.actions;
 export default store;
