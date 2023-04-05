9eed96bc2ff3958cffa7b0868decf55eaabd354c
syobochim <syobochim@gmail.com>
2023-04-05 21:29:03 +0900

    Add Cart using Redux and Toolkit

--- Chapter19/src/store/index.js
+++ Chapter19/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import UiSlice from "./ui-slice";
+import uiSlice from "./ui-slice";
+import cartSlice from "./cart-slice";
 
 const store = configureStore({
   reducer: {
-    ui: UiSlice.reducer,
+    ui: uiSlice.reducer,
+    cart: cartSlice.reducer,
   },
 });
 
