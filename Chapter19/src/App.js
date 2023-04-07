5bbd5393f9e52129b67b7259ce16539398470a3c
syobochim <syobochim@gmail.com>
2023-04-08 08:13:22 +0900

    Remove Unused import

--- Chapter19/src/App.js
+++ Chapter19/src/App.js
@@ -3,7 +3,6 @@ import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
 import { sendCartData } from "./store/cart-slice";
 
