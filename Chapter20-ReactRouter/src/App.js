3d0e1e6e466d85e5e94c476f3571117af6dcce29
syobochim <syobochim@gmail.com>
2023-04-12 13:42:17 +0900

    JSX Approach (React Router)

--- Chapter20-ReactRouter/src/App.js
+++ Chapter20-ReactRouter/src/App.js
@@ -1,11 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductPage from "./pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductPage /> },
-]);
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="products" element={<ProductPage />} />
+  </Route>
+);
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <ProductPage /> },
+// ]);
+
+const router = createBrowserRouter(routeDefinitions);
 
 function App() {
   return <RouterProvider router={router} />;
