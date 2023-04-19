a33d3cc135beca94c21e5d8048d40a3178faeaec
syobochim <syobochim@gmail.com>
2023-04-19 17:49:45 +0900

    loader()

--- Chapter20-ReactRouter2/frontend/src/App.js
+++ Chapter20-ReactRouter2/frontend/src/App.js
@@ -22,12 +22,12 @@
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
 import EventDetailPage from "./pages/EventDetailPage";
 import NewEventPage from "./pages/NewEventPage";
 import EditEventPage from "./pages/EditEventPage";
 import RootLayout from "./pages/Root";
 import EventRootLayout from "./pages/EventRoot";
+import EventsPage from "./pages/Events";
 
 const router = createBrowserRouter([
   {
@@ -39,7 +39,19 @@ const router = createBrowserRouter([
         path: "/events",
         element: <EventRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
