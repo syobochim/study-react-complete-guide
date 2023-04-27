419ab89ebc008f07e1ad3ebd4d53030f471d0b53
syobochim <syobochim@gmail.com>
2023-04-27 13:09:59 +0900

    Controlling which data should be deferred

--- Chapter20-ReactRouter2/frontend/src/components/EventsList.js
+++ Chapter20-ReactRouter2/frontend/src/components/EventsList.js
@@ -1,5 +1,5 @@
 import classes from "./EventsList.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function EventsList({ events }) {
   // const events = useLoaderData();
@@ -9,7 +9,7 @@ function EventsList({ events }) {
       <ul className={classes.list}>
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
-            <Link to={event.id}>
+            <Link to={`/events/${event.id}`}>
               <img src={event.image} alt={event.title} />
               <div className={classes.content}>
                 <h2>{event.title}</h2>
--- Chapter20-ReactRouter2/frontend/src/pages/EventDetailPage.js
+++ Chapter20-ReactRouter2/frontend/src/pages/EventDetailPage.js
@@ -1,15 +1,35 @@
 import EventItem from "../components/EventItem";
-import { json, redirect, useRouteLoaderData } from "react-router-dom";
+import {
+  Await,
+  defer,
+  json,
+  redirect,
+  useRouteLoaderData,
+} from "react-router-dom";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 const EventDetailPage = () => {
-  const data = useRouteLoaderData("event-detail");
-  return <EventItem event={data.event} />;
+  const { event, events } = useRouteLoaderData("event-detail");
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
 };
 
 export default EventDetailPage;
 
-export const eventDetailLoader = async ({ params }) => {
-  const id = params.eventId;
+const loadEvent = async (id) => {
   const response = await fetch("http://localhost:8080/events/" + id);
 
   if (!response.ok) {
@@ -18,8 +38,34 @@ export const eventDetailLoader = async ({ params }) => {
       { status: 500 }
     );
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.event;
   }
+};
+
+const loadEvents = async () => {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+    return json({ message: "Could not fetch events." }, { status: 500 });
+  } else {
+    const resData = await response.json();
+    console.log("response: ", resData.events);
+    return resData.events;
+  }
+};
+
+export const eventDetailLoader = async ({ params }) => {
+  const id = params.eventId;
+
+  return defer({
+    event: loadEvent(id),
+    events: loadEvents(),
+  });
 };
 
 export const deleteEventAction = async ({ params, request }) => {
