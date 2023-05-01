a1855fa01eea0671cb86fca3c83113f6ffb51356
syobochim <syobochim@gmail.com>
2023-05-01 11:47:31 +0900

    Adding user logout

--- Chapter21-Authentication/frontend/src/App.js
+++ Chapter21-Authentication/frontend/src/App.js
@@ -16,6 +16,7 @@ import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
+import { action as logoutAction } from "./pages/Logout";
 
 const router = createBrowserRouter([
   {
@@ -63,6 +64,7 @@ const router = createBrowserRouter([
         element: <NewsletterPage />,
         action: newsletterAction,
       },
+      { path: "logout", action: logoutAction },
     ],
   },
 ]);
