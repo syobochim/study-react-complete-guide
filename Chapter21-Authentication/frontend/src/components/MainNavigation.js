f5406fb7f6759a836ab3846b5741652fdb654dc3
syobochim <syobochim@gmail.com>
2023-05-02 13:21:24 +0900

    Managing the token expiration

--- Chapter21-Authentication/frontend/src/components/MainNavigation.js
+++ Chapter21-Authentication/frontend/src/components/MainNavigation.js
@@ -41,18 +41,19 @@ function MainNavigation() {
               Newsletter
             </NavLink>
           </li>
-          {!token && (
-            <li>
-              <NavLink
-                to="/auth?mode=login"
-                className={({ isActive }) =>
-                  isActive ? classes.active : undefined
-                }
-              >
-                Authentication
-              </NavLink>
-            </li>
-          )}
+          {!token ||
+            (token === "EXPIRED" && (
+              <li>
+                <NavLink
+                  to="/auth?mode=login"
+                  className={({ isActive }) =>
+                    isActive ? classes.active : undefined
+                  }
+                >
+                  Authentication
+                </NavLink>
+              </li>
+            ))}
           <li>
             <Form action="/logout" method="post">
               <button>Logout</button>
--- Chapter21-Authentication/frontend/src/util/auth.js
+++ Chapter21-Authentication/frontend/src/util/auth.js
@@ -1,7 +1,21 @@
 import { redirect } from "react-router-dom";
 
+export const getTokenDuration = () => {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+};
+
 export const getAuthToken = () => {
   const token = localStorage.getItem("token");
+
+  const tokenDuration = getTokenDuration();
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
 };
 
