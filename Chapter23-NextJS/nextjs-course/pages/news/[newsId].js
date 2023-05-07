b73209e0b19acfcb1711b4c1bc92caa5f2022527
syobochim <syobochim@gmail.com>
2023-05-07 14:38:50 +0900

    Extracting dynamic parameter values

--- Chapter23-NextJS/nextjs-course/pages/news/[newsId].js
+++ Chapter23-NextJS/nextjs-course/pages/news/[newsId].js
@@ -1,6 +1,12 @@
 // our-domain.com/news/something-important
 
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  const router = useRouter();
+  const newsId = router.query.newsId;
+  console.log(newsId);
+
   return <h1>The Detail Page</h1>;
 };
 
