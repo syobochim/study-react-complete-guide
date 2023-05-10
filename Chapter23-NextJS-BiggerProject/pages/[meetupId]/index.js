61737e23cc98a883b6c8c67232437c7c36cb16c4
syobochim <syobochim@gmail.com>
2023-05-10 16:09:13 +0900

    Adding custom components and css modules

---
+++ Chapter23-NextJS-BiggerProject/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Mont-Saint-Michel_vu_du_ciel.jpg/1920px-Mont-Saint-Michel_vu_du_ciel.jpg"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a First meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
