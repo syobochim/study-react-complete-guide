522979dc91147ba2bf8db0554aab40a79c1c8763
syobochim <syobochim@gmail.com>
2023-05-14 11:50:52 +0900

    Preparing paths with 'getStaticPaths'

--- Chapter23-NextJS-BiggerProject/pages/[meetupId]/index.js
+++ Chapter23-NextJS-BiggerProject/pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ const MeetupDetails = () => {
   );
 };
 
+export const getStaticPaths = async () => {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+};
+
+export const getStaticProps = async (context) => {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "First Meetup",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Mont-Saint-Michel_vu_du_ciel.jpg/1920px-Mont-Saint-Michel_vu_du_ciel.jpg",
+        address: "Some Street 5, Some City",
+        description: "This is a First meetup",
+      },
+    },
+  };
+};
+
 export default MeetupDetails;
