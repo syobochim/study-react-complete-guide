579e231288406b55b8aafb47f5bede96cb5e995f
syobochim <syobochim@gmail.com>
2023-05-19 14:23:59 +0900

    Adding "head" metadata

--- Chapter23-NextJS-BiggerProject/pages/new-meetup/index.js
+++ Chapter23-NextJS-BiggerProject/pages/new-meetup/index.js
@@ -2,6 +2,7 @@
 
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 const NewMeetupPage = () => {
   const router = useRouter();
@@ -18,7 +19,18 @@ const NewMeetupPage = () => {
 
     router.push("/");
   };
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <>
+      <Head>
+        <title>Add a Meetup</title>
+        <meta
+          name="description"
+          content="Add your own meetups and create amazing networking oppotunity!"
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </>
+  );
 };
 
 export default NewMeetupPage;
