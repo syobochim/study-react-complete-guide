7590bdeea3485353df36128da95705f6ee8f4dd9
syobochim <syobochim@gmail.com>
2023-05-27 22:25:54 +0900

    Animation timing

--- Chapter24-Animating/src/components/Modal/Modal.js
+++ Chapter24-Animating/src/components/Modal/Modal.js
@@ -3,6 +3,11 @@ import React from "react";
 import "./Modal.css";
 import { Transition } from "react-transition-group";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
   return (
     <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
