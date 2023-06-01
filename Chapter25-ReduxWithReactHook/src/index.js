e3ae063e0949f40e02303af8a884faf84d0ec40e
syobochim <syobochim@gmail.com>
2023-06-01 13:55:49 +0900

    Using the context API

--- Chapter25-ReduxWithReactHook/src/index.js
+++ Chapter25-ReduxWithReactHook/src/index.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
-import productReducer from './store/reducers/products';
+import "./index.css";
+import App from "./App";
+import ProductsProvider from "./context/products-context";
+import { createRoot } from "react-dom/client";
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
+const root = createRoot(document.getElementById("root"));
 
-const store = createStore(rootReducer);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
