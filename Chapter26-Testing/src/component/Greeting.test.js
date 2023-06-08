d49165fca1079c638e73a639f99e5a6abfab18cb
syobochim <syobochim@gmail.com>
2023-06-09 08:27:22 +0900

    Add test when button was clicked

--- Chapter26-Testing/src/component/Greeting.test.js
+++ Chapter26-Testing/src/component/Greeting.test.js
@@ -34,4 +34,19 @@ describe("Greeting component", () => {
     const outputElement = screen.getByText("Changed!", { exact: false });
     expect(outputElement).toBeInTheDocument();
   });
+
+  test('does not render "good to see you" if the button was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).not.toBeInTheDocument();
+  });
 });
