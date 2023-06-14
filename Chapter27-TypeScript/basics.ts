4c7b04befa9874d24fea3fb11544e3a7030f3c88
syobochim <syobochim@gmail.com>
2023-06-14 11:53:08 +0900

    Basic types

---
+++ Chapter27-TypeScript/basics.ts
@@ -0,0 +1,15 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function type, parameters
+
+// Primitives
+let age: number = 32;
+age = 33;
+
+let userName: string = "shobochim";
+userName = "syobochim";
+
+let isInstructor: boolean = true;
+isInstructor = false;
+
+let hobbies: null; // It can't be set to a non-null value.
